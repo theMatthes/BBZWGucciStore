a3a23a9b0f8d599b8a2c134fe97680cf4bb5147f
Mathias Planzer <mathias.planzer@kauz.ch>
2019-12-16 14:34:35 +0100

    Added Logo + More commits

--- src/app/products.service.ts
+++ src/app/products.service.ts
@@ -10,7 +10,13 @@ export class Product {
 export class ProductService {
   products: Array<Product> = [];
   constructor() {
-    this.products.push(new Product('Full outfit', 'img1.jpg', 3999.99));
-    this.products.push(new Product('Full outfit', 'img1.jpg', 3.14));
+    this.products.push(new Product('Wool alpaca jacquard cardigan', 'img1.jpg', 6280));
+    this.products.push(new Product('Tweed dress with detachable elements', 'img2.jpg', 4300));
+    this.products.push(new Product('Leather ankle boot with Sylvie Web', 'img3.jpg', 1200));
+    this.products.push(new Product('Loafers with specal elegance', 'img4.jpg', 1450));
+    this.products.push(new Product('Sun Glasses "Hater Blockers"', 'img5.jpg', 600));
+    this.products.push(new Product('Striped silk linen jacket with feline', 'img6.jpg', 2200));
+    this.products.push(new Product('Linen jacket with "Cassandra" patch', 'img7.jpg', 3250));
+    this.products.push(new Product('Gucci Bestiary backpack with tigers', 'img8.jpg', 1770));
    }
 }
